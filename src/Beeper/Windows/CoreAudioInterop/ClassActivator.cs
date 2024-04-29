998651b451fdc444235a2373e565e59dca5061c9
voytas <voytas@outlook.com>
2024-04-29 14:46:51 +0100

    Update activator

--- src/Beeper/Windows/CoreAudioInterop/ClassActivator.cs
+++ src/Beeper/Windows/CoreAudioInterop/ClassActivator.cs
@@ -1,29 +1,29 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace OldBit.Beeper.Windows.CoreAudioInterop
+namespace OldBit.Beeper.Windows.CoreAudioInterop;
+
+internal static class ClassActivator
 {
-    internal static class ClassActivator
+    private const int ClsCtxInprocServer = 0x01;
+
+    internal static I Activate<I>(string clsid, string iid) => Activate<I>(new Guid(clsid), new Guid(iid));
+
+    private static I Activate<I>(Guid clsid, Guid iid)
     {
-        public static I Activate<I>(string clsid, string iid) => Activate<I>(new Guid(clsid), new Guid(iid));
-
-        public static I Activate<I>(Guid clsid, Guid iid)
+        var result = CoCreateInstance(ref clsid, IntPtr.Zero, ClsCtxInprocServer, ref iid, out var obj);
+        if (result < 0)
         {
-            var result = CoCreateInstance(ref clsid, IntPtr.Zero, /*CLSCTX_INPROC_SERVER*/ 1, ref iid, out object obj);
-            if (result < 0)
-            {
-                Marshal.ThrowExceptionForHR(result);
-            }
-
-            return (I)obj;
+            Marshal.ThrowExceptionForHR(result);
         }
 
-        [DllImport("ole32")]
-        private static extern int CoCreateInstance(
-            ref Guid rclsid,
-            IntPtr pUnkOuter, 
-            uint dwClsContext, 
-            ref Guid riid, 
-            [MarshalAs(UnmanagedType.IUnknown)] out object ppObj);
-
+        return (I)obj;
     }
+
+    [DllImport("ole32")]
+    private static extern int CoCreateInstance(
+        ref Guid rclsid,
+        IntPtr pUnkOuter,
+        uint dwClsContext,
+        ref Guid riid,
+        [MarshalAs(UnmanagedType.IUnknown)] out object ppObj);
 }
