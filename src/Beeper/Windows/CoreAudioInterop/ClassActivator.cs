aea29be8fbf008b116bec7ff6f55c62c67daed93
Wojciech Sobieszek <voytas@outlook.com>
2024-04-29 15:34:09 +0100

    Use ComWrappers object creator

--- src/Beeper/Windows/CoreAudioInterop/ClassActivator.cs
+++ src/Beeper/Windows/CoreAudioInterop/ClassActivator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace OldBit.Beeper.Windows.CoreAudioInterop;
 
@@ -6,17 +7,20 @@ internal static class ClassActivator
 {
     private const int ClsCtxInprocServer = 0x01;
 
+    private static readonly ComWrappers _comWrappers = new StrategyBasedComWrappers();
+
     internal static I Activate<I>(string clsid, string iid) => Activate<I>(new Guid(clsid), new Guid(iid));
 
     private static I Activate<I>(Guid clsid, Guid iid)
     {
         var result = CoCreateInstance(ref clsid, IntPtr.Zero, ClsCtxInprocServer, ref iid, out var obj);
+        
         if (result < 0)
         {
             Marshal.ThrowExceptionForHR(result);
         }
 
-        return (I)obj;
+        return (I)_comWrappers.GetOrCreateObjectForComInstance(obj, CreateObjectFlags.None);
     }
 
     [DllImport("ole32")]
@@ -25,5 +29,5 @@ internal static class ClassActivator
         IntPtr pUnkOuter,
         uint dwClsContext,
         ref Guid riid,
-        [MarshalAs(UnmanagedType.IUnknown)] out object ppObj);
+        out IntPtr ppObj);
 }
