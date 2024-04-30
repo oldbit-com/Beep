389849f8d62f86bf4fbc369de13f1d1ece82b11c
Wojciech Sobieszek <voytas@outlook.com>
2024-04-30 15:52:12 +0100

    Add missing class

---
+++ src/Beeper/Windows/CoreAudioInterop/SubFormat.cs
@@ -0,0 +1,8 @@
+﻿namespace OldBit.Beeper.Windows.CoreAudioInterop;
+
+internal static class SubFormat
+{
+    internal static readonly Guid Pcm = new("00000001-0000-0010-8000-00AA00389B71");
+
+    internal static readonly Guid IeeeFloat = new("00000003-0000-0010-8000-00aa00389b71");
+}
