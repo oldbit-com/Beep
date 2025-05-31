2e2bf8feb38fc61f1e98d9347719439245a1c0e9
Wojciech Sobieszek <voytas@outlook.com>
2025-05-31 12:25:34 +0100

    Move queue logic to separate class (#4)

---
+++ src/Beep/Platforms/MacOS/AudioBuffer.cs
@@ -0,0 +1,18 @@
+namespace OldBit.Beep.Platforms.MacOS;
+
+internal class AudioBuffer
+{
+    private static readonly TimeSpan AudioQueueCallbackTimeout = TimeSpan.FromSeconds(15);
+
+    internal readonly IntPtr Pointer;
+
+    internal DateTimeOffset LastAccessTime { get; set; }
+
+    internal bool IsStale => DateTimeOffset.UtcNow - LastAccessTime > AudioQueueCallbackTimeout;
+
+    internal AudioBuffer(IntPtr pointer)
+    {
+        Pointer = pointer;
+        LastAccessTime = DateTimeOffset.UtcNow;
+    }
+}
